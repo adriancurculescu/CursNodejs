159e619d5a6594566df89bfd094ab3f27442ac10
Adrian <adrian.curculescu@yahoo.com>
2017-07-25 12:28:56 +0300

    update: tests with implementations

--- UnitTestExample/index.js
+++ UnitTestExample/index.js
@@ -1,7 +1,7 @@
 console.log("Unit test log example starts");
 
-var password = "testBauBau2017";
+var password = "test";
 var myValidator = require("./modules/passwordValidator");
 var valid = myValidator.validPassword(password);
 
-console.log("Your password '" + password + "' is: " + valid);
+console.log("Your password '" + password + "' is valid --> " + valid);
--- UnitTestExample/test/passwordValidatorTest.js
+++ UnitTestExample/test/passwordValidatorTest.js
@@ -1,3 +1,7 @@
+var chai = require("chai");
+var passwordValidator = require("../modules/passwordValidator");
+
+
 //module level
 describe("passwordValidator", function () {
 
@@ -8,6 +12,39 @@ describe("passwordValidator", function () {
         it("should have minimum 8 charaters", function () {
 
             //Test Goes Here
+
+            //implementetion test with known wrong value
+            var wrongPass = "1234567";
+            var valid = passwordValidator.validPassword(wrongPass);
+            
+            //chai check
+            chai.expect(valid).to.be.equal(false);
+
+
+            //test with good value
+            var goodPass = "12345678";
+            var valid2 = passwordValidator.validPassword(goodPass);
+            chai.expect(valid).to.be.equal(true);
+
+        });
+
+        //function test 2
+        it("should not contains <password> inside", function () {
+
+            //Test Goes Here
+
+            //implementetion test with known wrong value
+            var wrongPass = "password1";
+            var valid2 = passwordValidator.validPassword(wrongPass);
+            
+            //chai check
+            chai.expect(valid2).to.be.equal(false);
+
+
+            //test with good value
+            var goodPass = "bla-bla-bla-bla";
+            var valid2 = passwordValidator.validPassword(goodPass);
+            chai.expect(valid2).to.be.equal(true);
 
         });
 
